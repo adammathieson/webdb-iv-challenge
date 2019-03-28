08dcdc5efb62043b1129bd1221928fd0b2cc118a
Adam Mathieson <mathieson1222@gmail.com>
2019-03-28 14:16:03 -0700

    adds knexfile.js and initial router setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const dishesRouter = require('../routers/dishes-router.js');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.use('/api/dishes', dishesRouter);
 
 module.exports = server;
---
+++ routers/dishes-router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+
+const db = knex(knexConfig.development);
+
+router.get('/', (req, res) => {
+    db('dishes')
+        .then(dishes => {
+            res.status(200).json(dishes);
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
+});
+
+module.exports = router;
